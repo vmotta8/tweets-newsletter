e3238233f3cc32528e480970beb3656327e10246
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-16 19:28:52 -0300

    feat: add jest

---
+++ user-service/jest.config.js
@@ -0,0 +1,13 @@
+const { resolve } = require('path')
+const root = resolve(__dirname)
+
+module.exports = {
+  rootDir: root,
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/test/$1'
+  }
+}

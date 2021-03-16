13b733ad372a618da274da5ed2e36601f9c2a749
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-16 11:03:25 -0300

    feat: create user service

---
+++ user-service/src/handlers/createUser.js
@@ -0,0 +1,8 @@
+async function createUser(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
+  };
+}
+
+export const handler = createUser;

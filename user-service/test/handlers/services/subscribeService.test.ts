1afd5f5febc592d4fbedc2dbf130855b65668be6
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-17 18:02:23 -0300

    feat: add in memory repositories

--- user-service/test/handlers/services/subscribeService.test.ts
+++ user-service/test/handlers/services/subscribeService.test.ts
@@ -1,35 +1,59 @@
 import { SubscribeService } from '../../../src/handlers/services/subscribeService'
 import { IMailProvider, IMessage } from '../../../src/lib/providers/IMailProvider'
+import { IRepository } from '../../../src/lib/repositories/IRepository'
+import { User } from '../../../src/entities/User'
 
 export class SESMailProviderInMemory implements IMailProvider {
   async sendMessage (message: IMessage): Promise<void> {
     //
   }
 }
+
+export class DynamoRepositoryInMemory implements IRepository {
+  private db: User[]
+  constructor () {
+    this.db = []
+  }
+
+  async findByEmail (email: string): Promise<boolean> {
+    const result = this.db.find(user => user.email === email)
+    if (result) {
+      return true
+    }
+
+    return false
+  }
+
+  async save (user: User): Promise<void> {
+    this.db.push(user)
+  }
+}
+
 const service = new SubscribeService(
-  new SESMailProviderInMemory()
+  new SESMailProviderInMemory(),
+  new DynamoRepositoryInMemory()
 )
 
 describe('subscribe service', () => {
-  it('should return the right email', () => {
+  it('should return the right email', async () => {
     process.env.MAIL_QUEUE_URL = 'https://any.com'
     const data = {
       email: 'email@gmail.com'
     }
-    const response = service.execute(data)
+    const response = await service.execute(data)
     expect(response.email).toBe('email@gmail.com')
   })
 
-  it('should return an invalid url error', () => {
-    process.env.MAIL_QUEUE_URL = ''
+  it('should return an error if email already exists', async () => {
+    process.env.MAIL_QUEUE_URL = 'https://any.com'
     const data = {
       email: 'email@gmail.com'
     }
     try {
-      service.execute(data)
+      await service.execute(data)
       expect(true).toBe(false)
     } catch (error) {
-      expect(error.message).toBe('Invalid queue url.')
+      expect(error.message).toBe('Email already exists.')
     }
   })
 })

7449a3b0abaf772762c8f8e169b96a614a3b6859
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-18 17:13:44 -0300

    test: fix send service

--- twitter-service/test/handlers/services/sendService.test.ts
+++ twitter-service/test/handlers/services/sendService.test.ts
@@ -7,7 +7,7 @@ const service = new SendService(
 
 describe('send service', () => {
   it('should return tweets', async () => {
-    const users = await service.execute({ email: 'viniciusmotta8@gmail.com' })
-    expect(users).toEqual([{ email: 'active@email.com', status: 'ACTIVE' }])
+    const response = await service.execute({ email: 'viniciusmotta8@gmail.com' })
+    expect(response.message).toEqual('News sent successfully!')
   })
 })

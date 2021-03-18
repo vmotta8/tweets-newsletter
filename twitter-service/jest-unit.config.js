ef9c891b0315d6777ce6bf478c57e1277bcfe6eb
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-18 15:09:19 -0300

    feat: add token helper

--- twitter-service/jest-unit.config.js
+++ twitter-service/jest-unit.config.js
@@ -1,6 +1,6 @@
 const config = require('./jest.config')
 
 config.displayName = 'unit-tests'
-config.testMatch = ['<rootDir>/src/**/*.test.ts']
+config.testMatch = ['<rootDir>/test/**/*.test.ts']
 
 module.exports = config
---
+++ twitter-service/src/helpers/tokenHelper.ts
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import qs from 'querystring'
+import createError from 'http-errors'
+
+export const tokenHelper = {
+  async generate (): Promise<any | false> {
+    const url = process.env.AUTH_URL
+    const id = process.env.AUTH_ID
+    const username = process.env.AUTH_USERNAME
+    const password = process.env.AUTH_PASSWORD
+
+    if (url === undefined || id === undefined || username === undefined || password === undefined) {
+      console.log(url, id, username, password)
+      throw new createError.BadRequest('Wrong envs.')
+    }
+
+    const data = {
+      client_id: id,
+      username: username,
+      password: password,
+      grant_type: 'password',
+      scope: 'openid'
+    }
+
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    const response = await axios.post(url,
+      qs.stringify(data),
+      {
+        headers: headers
+      })
+
+    return response.data
+  }
+}

9ab5ae2ecb6260b2471d00007b4b21177134deeb
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-18 00:57:55 -0300

    test: improve subscribe service test

--- user-service/test/handlers/services/subscribeService.test.ts
+++ user-service/test/handlers/services/subscribeService.test.ts
@@ -1,37 +1,6 @@
 import { SubscribeService } from '../../../src/handlers/services/subscribeService'
-import { IMailProvider, IMessage } from '../../../src/lib/providers/IMailProvider'
-import { IRepository } from '../../../src/lib/repositories/IRepository'
-import { User } from '../../../src/entities/User'
-
-export class SESMailProviderInMemory implements IMailProvider {
-  async sendMessage (message: IMessage): Promise<void> {
-    //
-  }
-}
-
-export class DynamoRepositoryInMemory implements IRepository {
-  private db: User[]
-  constructor () {
-    this.db = []
-  }
-
-  async findAll (): Promise<any> {
-    return this.db
-  }
-
-  async findByEmail (email: string): Promise<false | any> {
-    const result = this.db.find(user => user.email === email)
-    if (result) {
-      return true
-    }
-
-    return false
-  }
-
-  async save (user: User): Promise<void> {
-    this.db.push(user)
-  }
-}
+import { SESMailProviderInMemory } from '../../lib/providers/SESMailProviderInMemory'
+import { DynamoRepositoryInMemory } from '../../lib/repositories/DynamoRepositoryInMemory'
 
 const service = new SubscribeService(
   new SESMailProviderInMemory(),
@@ -39,25 +8,32 @@ const service = new SubscribeService(
 )
 
 describe('subscribe service', () => {
+  process.env.MAIL_QUEUE_URL = 'https://any.com'
   it('should return the right email', async () => {
-    process.env.MAIL_QUEUE_URL = 'https://any.com'
     const data = {
-      email: 'email@gmail.com'
+      email: 'email@email.com'
     }
     const response = await service.execute(data)
-    expect(response.email).toBe('email@gmail.com')
+    expect(response.message).toBe('You have been subscribed. Be welcome!!')
   })
 
-  it('should return an error if email already exists', async () => {
-    process.env.MAIL_QUEUE_URL = 'https://any.com'
+  it('should activate the user if he is inactive', async () => {
     const data = {
-      email: 'email@gmail.com'
+      email: 'inactive@email.com'
+    }
+    const response = await service.execute(data)
+    expect(response.message).toBe('You have been subscribed. Welcome back!!')
+  })
+
+  it('should return an error if user already subscribed', async () => {
+    const data = {
+      email: 'email@email.com'
     }
     try {
       await service.execute(data)
       expect(true).toBe(false)
     } catch (error) {
-      expect(error.message).toBe('Email already exists.')
+      expect(error.message).toBe('You are already subscribed, enjoy the news !!')
     }
   })
 })

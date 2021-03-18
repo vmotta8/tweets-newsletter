391468b1af454cc6e4b662c330a462a5f410f508
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-18 00:18:07 -0300

    feat: add unsubscribe service

--- user-service/src/handlers/services/unsubscribeService.ts
+++ user-service/src/handlers/services/unsubscribeService.ts
@@ -1,12 +1,23 @@
 import { IRepository } from '../../lib/repositories/IRepository'
 import { IUnsubscribeDTO } from './dtos/IUnsubscribeDTO'
+import createError from 'http-errors'
 
 export class UnsubscribeService {
   constructor (
     private repository: IRepository
   ) {}
 
-  async execute (data: IUnsubscribeDTO): Promise<{ message: string; }> {
-    return { message: 'OK' }
+  async execute (data: IUnsubscribeDTO): Promise<any> {
+    const user = await this.repository.findByEmail(data.email)
+    if (!user) {
+      throw new createError.BadRequest('Email not found.')
+    }
+
+    if (user.status === 'INACTIVE') {
+      throw new createError.BadRequest('You have already unsubscribed, but you will be welcome if you want to come back !!')
+    }
+
+    await this.repository.changeStatus(user.id, 'INACTIVE')
+    return { message: 'Unsubscribed successfully!' }
   }
 }

9b83ec766cbd925811652f11de5177eea37bc265
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-19 09:52:13 -0300

    refactor: use get to access the route through the browser

--- user-service/src/handlers/subscribe.ts
+++ user-service/src/handlers/subscribe.ts
@@ -1,8 +1,6 @@
 import { APIGatewayProxyResult } from 'aws-lambda'
 import { commonMiddleware } from '../lib/middlewares/commonMiddleware'
 import { SubscribeService } from './services/subscribeService'
-import validator from '@middy/validator'
-import subscribeSchema from '../lib/schemas/subscribeSchema'
 import { SESMailProvider } from '../lib/providers/implementations/SESMailProvider'
 import { DynamoRepository } from '../lib/repositories/implementations/DynamoRepository'
 
@@ -12,7 +10,7 @@ const service = new SubscribeService(
 )
 
 async function subscribe (event: any, context: any): Promise<APIGatewayProxyResult> {
-  const response = await service.execute(event.body)
+  const response = await service.execute(event.pathParameters)
   return {
     statusCode: 200,
     body: JSON.stringify(response)
@@ -20,4 +18,3 @@ async function subscribe (event: any, context: any): Promise<APIGatewayProxyResu
 }
 
 export const handler = commonMiddleware(subscribe)
-  .use(validator({ inputSchema: subscribeSchema }))

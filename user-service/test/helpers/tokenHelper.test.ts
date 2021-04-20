b245a60b552c770ad4eb5e57c02077051ba51cac
Vinicius Motta <viniciusmotta8@gmail.com>
2021-04-20 12:02:02 -0300

    refactor: remove auth0

--- user-service/test/helpers/tokenHelper.test.ts
+++ user-service/test/helpers/tokenHelper.test.ts
@@ -1,11 +1,6 @@
 import { tokenHelper } from '../../src/helpers/tokenHelper'
 
 describe('token helper', () => {
-  it('should return a bearer token', async () => {
-    const data = await tokenHelper.generateauth0()
-    expect(data.token_type).toBe('Bearer')
-  })
-
   it('should return a cognito bearer token', async () => {
     const data = await tokenHelper.generate()
     expect(typeof data).toBe('string')

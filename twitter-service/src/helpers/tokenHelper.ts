a02b97d221239bb176d3f3c40fc5bdc28d3c5bef
Vinicius Motta <viniciusmotta8@gmail.com>
2021-04-20 12:23:04 -0300

    refactor: remove auth0

--- twitter-service/src/helpers/tokenHelper.ts
+++ twitter-service/src/helpers/tokenHelper.ts
@@ -1,44 +1,6 @@
 import axios from 'axios'
-import qs from 'querystring'
-import createError from 'http-errors'
 
 export const tokenHelper = {
-  async generateauth0 (): Promise<any> {
-    const url = process.env.AUTH_URL
-    const id = process.env.AUTH_ID
-    const username = process.env.AUTH_USERNAME
-    const password = process.env.AUTH_PASSWORD
-
-    if (url === undefined || id === undefined || username === undefined || password === undefined) {
-      console.log(url, id, username, password)
-      throw new createError.BadRequest('Wrong envs.')
-    }
-
-    const data = {
-      client_id: id,
-      username: username,
-      password: password,
-      grant_type: 'password',
-      scope: 'openid'
-    }
-
-    const headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    try {
-      const response = await axios.post(url,
-        qs.stringify(data),
-        {
-          headers: headers
-        })
-      return response.data
-    } catch (err) {
-      console.log(err)
-      throw new createError.Unauthorized('Wrong credentials')
-    }
-  },
-
   async generate (): Promise<any> {
     const url = `${process.env.COGNITO_URL}${process.env.COGNITO_REFRESH_TOKEN}`
 
--- twitter-service/test/helpers/tokenHelper.test.ts
+++ twitter-service/test/helpers/tokenHelper.test.ts
@@ -1,31 +1,6 @@
 import { tokenHelper } from '../../src/helpers/tokenHelper'
 
 describe('token helper', () => {
-  it('should return a bearer token', async () => {
-    const data = await tokenHelper.generateauth0()
-    expect(data.token_type).toBe('Bearer')
-  })
-
-  it('should return unauthorized error', async () => {
-    process.env.AUTH_ID = '0000'
-    try {
-      await tokenHelper.generateauth0()
-      expect(true).toBe(false)
-    } catch (err) {
-      expect(err.message).toBe('Wrong credentials')
-    }
-  })
-
-  it('should return bad request error', async () => {
-    try {
-      delete process.env.AUTH_ID
-      await tokenHelper.generateauth0()
-      expect(true).toBe(false)
-    } catch (err) {
-      expect(err.message).toBe('Wrong envs.')
-    }
-  })
-
   it('should return a cognito bearer token', async () => {
     const data = await tokenHelper.generate()
     expect(data.token_type).toBe('Bearer')

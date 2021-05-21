f7f4b943483f35bca39f38401498ea2675de409d
Vinicius Motta <viniciusmotta8@gmail.com>
2021-05-21 00:14:00 -0300

    refactor: user entity

--- user-service/src/entities/User.ts
+++ user-service/src/entities/User.ts
@@ -1,22 +1,13 @@
 import { uuidHelper } from '../helpers/uuidHelper'
-import { emailValidator } from './validators/emailValidator'
-import createError from 'http-errors'
 
 export class User {
   public readonly id: string;
   public readonly status: string;
-
   public email: string;
 
   constructor (props: Omit<User, 'id' | 'status'>) {
     this.id = uuidHelper.create()
     this.status = 'ACTIVE'
-
-    if (emailValidator(props.email)) {
-      this.email = props.email
-    } else {
-      console.log(props.email)
-      throw new createError.NotAcceptable('Invalid email.')
-    }
+    this.email = props.email
   }
 }
